58e6fa101e15389b5c2ab5a13f158631031527d4
Lenyn Alcantara <nightzpy@gmail.com>
2017-10-09 03:01:16 -0400

    Adding rows and cols enemies completed

--- main/Flower.js
+++ main/Flower.js
@@ -3,13 +3,14 @@ function Flower(xPos, yPos) {
   this.y = yPos == 0 ? 20 : yPos;
   this.xSpeed = 1;
   this.height = ( height - 20 + 360 ) / (4 * 5);
-  this.width = ( width - 50 ) / 8;
+  this.width = ( width - 180 ) / 8;
   
   this.show = function () {
     console.log(xPos);
     //console.log( (this.x + this.width) + ', ' + (this.y + this.height) + ', ' + this.width + ', ' + this.height );
     fill ( 255, 120, 180 );
     ellipse ( this.x + this.width, this.y + this.height, this.width, this.height );
+    console.log ( this.width + ', ' + this.height );
   }
   
   this.move = function ( direction ) {
--- main/main.js
+++ main/main.js
@@ -11,24 +11,8 @@ var topOffset = 20;
 function setup() {
   createCanvas (sceneWidth, sceneHeight)
   ship = new Ship();
-  
-  for (var i = 0; i < flowersRow; i ++) {
-    flowers[i] = []
-    for (var j = 0; j < flowersCol; j ++) {
-      var flowerX = 0;
-      var flowerY = 0;
-      if ( i == 0 && j == 0 ) {
-        flowerX = leftOffset;
-        flowerY = topOffset;
-      } else {
-        if ( i == 0 && j > 0 ) {
-          flowerX = flowers[i][j - 1].x + (flowers[i][j - 1].width + 5);
-          flowerY = topOffset;
-        }
-      }      
-      flowers[i][j] = new Flower( flowerX, flowerY);     
-    }
-  }
+  initFlowers();
+
 }
 
 function draw() {
@@ -48,5 +32,31 @@ function keyPressed () {
     ship.right();
   } else if ( keyCode === LEFT_ARROW ) {
     ship.left();
+  }
+}
+
+function initFlowers () {
+  for (var i = 0; i < flowersRow; i ++) {
+    flowers[i] = []
+    for (var j = 0; j < flowersCol; j ++) {
+      var flowerX = 0;
+      var flowerY = 0;
+      if ( i == 0 && j == 0 ) {
+        flowerX = leftOffset;
+        flowerY = topOffset;
+      } else {
+        if ( i == 0 && j > 0 ) {
+          flowerX = flowers[i][j - 1].x + (flowers[i][j - 1].width + 5);
+          flowerY = topOffset;
+        } else if ( i > 0 && j == 0 ) {
+          flowerX = leftOffset;
+          flowerY = flowers[i - 1][j].y + (flowers[i - 1][j].height + 5);
+        } else {
+          flowerY = flowers[i - 1][j].y + (flowers[i - 1][j].height + 5);
+          flowerX = flowers[i][j - 1].x + (flowers[i][j - 1].width + 5);
+        }
+      }      
+      flowers[i][j] = new Flower( flowerX, flowerY);     
+    }
   }
 }
